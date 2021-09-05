112b9b1701954c2f9bcd15efd7bdf7f290da337b
natepace <n8wpace@gmail.com>
2021-09-05 17:27:43 -0400

    added map

--- src/components/PageEnder.js
+++ src/components/PageEnder.js
@@ -5,7 +5,15 @@ export const PageEnder = () => {
         <div>
             <ContactForm />
             <div className="contact-info-map">
-
+                <div className="address-box">
+                    <div className="address">
+                        <h3>The Pace Law Firm</h3>
+                        <p>2469 E. Ft. Union Blvd., Suite 112</p>
+                        <p>Cottonwood Heights, UT 84121</p>
+                        <p>Phone: 801-355-9700</p>
+                        <p>Fax: 801-355-9705</p>
+                    </div>
+                </div>
             </div>
             <div className="call-today">
                 <h3>Call Today for Your Complimentary Consultation</h3>
