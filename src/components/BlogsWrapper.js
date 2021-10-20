4ce925991e57fa529833031b3ec18f940b64a1ee
natepace <n8wpace@gmail.com>
2021-10-19 20:26:31 -0400

    pushed blog changes

--- src/components/BlogsWrapper.js
+++ src/components/BlogsWrapper.js
@@ -10,12 +10,27 @@ export const BlogsWrapper = (props) => {
 
     }
 
+    const container = []
+    if (blogEntry.entry != undefined) {
+        const splitUp = blogEntry.entry.split("\n")
+
+        splitUp.map(s => {
+            container.push(s.split(/[{}]/).filter(function (e) { return e; }))
+        })
+    }
+    console.log(container)
 
     return (
         <div>
             <h1>{blogEntry.title}</h1>
 
-            <p>{blogEntry.entry}</p>
+            {/* <change this to a container map for the paragraphs */}
+            {/* <p>{blogEntry.entry}</p> */}
+            <div
+                dangerouslySetInnerHTML={{
+                    __html: blogEntry.entry
+                }} >
+            </div>
 
         </div>
     )
